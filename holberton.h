3e74ae5ba0c2d82d820fd78b204137903ff8e855
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-16 22:48:44 +0000

    prototypes

--- holberton.h
+++ holberton.h
@@ -2,60 +2,32 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct Pf_func - Entry point
+ * Desc: Entry
+ *@f: pointer
+ *@a: int value
+ *@b: int value
+ *@op: char pointer
  */
 typedef struct Pf_func
 {
-        char *op;
+	char *op;
+	int (*f)(va_list);
+} print_fun;
 
-        int (*f)(int a, int b);
-} pffun;
+int _printf(const char *format, ...); /*prototype printf*/
+int _strcmp(const char *s1, char *s2);
+int (*get_op_func(const char *s, int pos))(va_list);
+int _putchar(char c); /*prototype putchar*/
 
-int _putchar(char c);
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar(char c);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
-char *_strstr(char *haystack, char *needle);
-void print_diagsums(int *a, int size);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));/*0x0F*/
+int print_single_char(va_list c);
+int print_string_char(va_list s);
 
+/*prototype function that compares two strings.*/
 #endif /* HOLBERTON_H */
