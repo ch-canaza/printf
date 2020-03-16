31bd2c990a072340f99c157d99f6efe25f7c71e4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-16 22:46:04 +0000

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,38 +1,70 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <unistd.h>
-
+/**
+ * _printf - Entry point
+ * Desc: Entry
+ *@format: pointer
+ * Return: On success.
+ */
 int _printf(const char *format, ...)
 {
+	va_list mylist;
+	unsigned int i=0, j = 0;
+	
+	if (!format)
+	{
+		return (-1);
+	}
+	if (format[0] == '%' && format[1] == '\0')
+	{
+		return (-1);
+	}
+	va_start(mylist, format);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	pffun options []{
-
-		{"%c", print_single_char},
-		{"%d", print_decimal},
-		{"%e", print_ex_floating_point},
-		{"%f", print_floating_point},
-		{"%g", print_general_format_float},
-		{"%i", print_integer},
-		{"%o", print_octal_number},
-		{"%s", print_string_char},
-		{"%u", print_unsigned_dec},
-		{"%x", print_number_hex},
-		{"%%", print_percent_sign},
-
+	for (i=0; format[i] != '\0'; i++)
+	{
+		if(format[i]== '%')
+		{
+			if(format[i + 1] == '%')
+			{
+				i ++;
+				_putchar ('%');
+			}
+			else if (get_op_func(format, i + 1) != NULL)
+			{
+				j +=(get_op_func(format, i + 1))(mylist);
+				i++;
+			}
+		}
+		else
+		{	
+			_putchar(format[i]);
+			j++;
+		}
+	}
+	va_end(mylist);
+	return (j);
 }
+/**
+ * get_op_func - Entry function
+ * @s: operator
+ * Return: function
+ */
+int (*get_op_func(const char *s, int pos))(va_list)
+{
+	print_fun ops[] = {
+		{"c", print_single_char},
+		{"s", print_string_char},
+		{NULL, NULL}
+	};
+	int k;
+
+	for (k = 0 ; ops[k].op != NULL; k++)
+	{
+		if (ops[k].op[0] == s[pos])
+		{
+			return (ops[k].f);
+		}
+	}
+	return (NULL);
+}
+	
